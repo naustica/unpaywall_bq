bbc358f4e7c80ca7b835c0d2791d53e166fe5c9d
Nick Haupka <nick.haupka@outlook.de>
2025-06-23 16:29:22 +0200

    update script

--- upw_export_jq_hpc.sh
+++ upw_export_jq_hpc.sh
@@ -3,38 +3,21 @@
 #SBATCH -C scratch
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -c 6
+#SBATCH -c 8
 #SBATCH -t 03:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nick.haupka@sub.uni-goettingen.de
 
-echo -n 'Start: ' > /scratch/${WORKING_DIRECTORY}/times.txt
-uptime >> /scratch/${WORKING_DIRECTORY}/times.txt
 
-module load jq/1.6
-
-echo -n 'Modules jq and gsutil loaded, create working directory: ' >> /scratch/${WORKING_DIRECTORY}/times.txt
-uptime >> /scratch/${WORKING_DIRECTORY}/times.txt
+module load jq
 
 mkdir /scratch/${WORKING_DIRECTORY}/${SNAPSHOT_DIRECTORY}
 
-echo -n 'Working directory created, start exporting: ' >> /scratch/${WORKING_DIRECTORY}/times.txt
-uptime >> /scratch/${WORKING_DIRECTORY}/times.txt
-
-zcat /scratch/${WORKING_DIRECTORY}/${SNAPSHOT_FILE} | parallel --pipe --block 100M --jobs 6 --files --tmpdir /scratch/${WORKING_DIRECTORY}/${SNAPSHOT_DIRECTORY} --recend '}\n' "jq -c 'select(.year >= 2008 and .year <= 2023) | {doi, genre, has_repository_copy, is_paratext, is_oa, journal_is_in_doaj, journal_is_oa, journal_issn_l, journal_issns, journal_name, oa_locations, oa_locations_embargoed, oa_status, publisher, published_date, doi_updated: .updated, year}'"
-
-echo -n 'Export finished, start renaming: ' >> /scratch/${WORKING_DIRECTORY}/times.txt
-uptime >> /scratch/${WORKING_DIRECTORY}/times.txt
+zcat /scratch/${WORKING_DIRECTORY}/${SNAPSHOT_FILE} | parallel --pipe --block 100M --jobs 8 --files --tmpdir /scratch/${WORKING_DIRECTORY}/${SNAPSHOT_DIRECTORY} --recend '}\n' "jq -c 'select(.year >= 2008 and .year <= 2025) | {doi, genre, has_repository_copy, is_paratext, is_oa, journal_is_in_doaj, journal_is_oa, journal_issn_l, journal_issns, journal_name, oa_locations, oa_locations_embargoed, oa_status, publisher, published_date, doi_updated: .updated, year}'"
 
 for file in /scratch/${WORKING_DIRECTORY}/${SNAPSHOT_DIRECTORY}/*.par
 do
   mv "$file" "${file%.par}.jsonl"
 done
 
-echo -n 'Renaming finished, start compressing: ' >> /scratch/${WORKING_DIRECTORY}/times.txt
-uptime >> /scratch/${WORKING_DIRECTORY}/times.txt
-
 gzip -r /scratch/${WORKING_DIRECTORY}/${SNAPSHOT_DIRECTORY}
-
-echo -n 'Script finished.' >> /scratch/${WORKING_DIRECTORY}/times.txt
-uptime >> /scratch/${WORKING_DIRECTORY}/times.txt
